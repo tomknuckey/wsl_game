0ffa75b1a2491b742939ff768b9f1d1b6ee5b1a2
Tom Knuckey <tomknuckey@hotmail.com>
2026-09-19 14:58:00 +0100

    Change location of manual updates

--- config.py
+++ config.py
@@ -18,3 +18,12 @@ players_to_fill_with: list = ["Khadija Shaw", "Alessia Russo", "Kirsty Hanson",
 
 #Maximum Number of gameweeks to filter on
 max_gw = 30
+
+# Manual mapping of player name corrections
+# Use exact matches on the cleaned `full_name` field in the long-form dataframe.
+MANUAL_RENAMES = {
+    "Laura James": "Lauren James",
+    "Bunny Shaw": "Khadija Shaw",
+    "Viviane Miedema": "Vivianne Miedema",
+    "Nadia krezyman": "Nadia Krezyman"
+}
