821f8d4d76de50d6c5213a7cfaa4ef1f7965f18a
Tom Knuckey <tomknuckey@hotmail.com>
2026-09-19 15:42:01 +0100

    Adding more to the app

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.title("WSL Fantasy Game")
 
@@ -6,4 +7,19 @@ st.write("Welcome to the WSL Fantasy Game!")
 
 st.header("Leaderboard")
 
+pdf_results = pd.read_csv("data/output/actual/results.csv")
+
+pdf_pics = pd.read_csv("data/output/actual/player_pics.csv")
+
+pdf_goals_agg = pd.read_csv("data/output/actual/pdf_goals_agg.csv")
+
+pdf_manager_ownership = pd.read_csv("data/output/actual/manager_ownership.csv")
 st.write("Results coming soon...")
+
+st.write(pdf_results)
+
+st.write(pdf_pics)
+
+st.write(pdf_goals_agg)
+
+st.write(pdf_manager_ownership)
--- readme.md
+++ readme.md
@@ -5,3 +5,5 @@
 2. .\venv\Scripts\Activate.ps1
 
 3. pip install -r requirements.txt
+
+streamlit run app.py
